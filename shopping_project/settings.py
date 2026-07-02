4b3cb4d6f8448bf4368524a20db58b0fb275e881
shivkumar27-sb <shiv.biradar272003@gmail.com>
2026-07-02 15:26:32 +0530

    Fix media root for uploaded product images

--- shopping_project/settings.py
+++ shopping_project/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/image/download/'
-MEDIA_ROOT = BASE_DIR 
+MEDIA_ROOT = BASE_DIR / "store"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

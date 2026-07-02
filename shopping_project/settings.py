9dbd0737bc5d778f9c4aa8cec6493b6070c35da9
shivkumar27-sb <shiv.biradar272003@gmail.com>
2026-07-02 17:56:14 +0530

    settings python

--- shopping_project/settings.py
+++ shopping_project/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "upload"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

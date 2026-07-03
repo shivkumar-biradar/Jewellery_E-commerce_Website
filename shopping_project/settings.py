d2f67473d9e1803efb978a6821d7cf937a9f7384
shivkumar27-sb <shiv.biradar272003@gmail.com>
2026-07-03 15:44:53 +0530

    Use Django 5 STORAGES for Cloudinary

--- shopping_project/settings.py
+++ shopping_project/settings.py
@@ -146,4 +146,11 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
